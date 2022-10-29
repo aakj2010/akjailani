66209f319bd63793bb5f06a6910264369111ebbe
aakj2010 <aakj2010@gmail.com>
2022-10-29 16:31:11 +0530

    AWS

--- src/Component/skill/Skills.jsx
+++ src/Component/skill/Skills.jsx
@@ -14,6 +14,7 @@ import github from '../Asset/github.png'
 import ex from '../Asset/ex.png'
 import heroku from '../Asset/heroku.png'
 import netlify from '../Asset/netlify.png'
+import AWS from '../Asset/AWS.png'
 
 
 function Skills() {
@@ -61,6 +62,9 @@ function Skills() {
         },
         {
             icon: <img src={netlify} alt="" className="HTML" />
+        },
+        {
+            icon: <img src={AWS} alt="" className="HTML" />
         }
     ]
 
